ff7c26ae7396a6c8c50fbae444ff46fd8989feb7
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-02-11 17:24:06 -0600

    Initial Commit

---
+++ Shared/Neutron_ShieldingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neutron_ShieldingApp.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Neutron_ShieldingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
